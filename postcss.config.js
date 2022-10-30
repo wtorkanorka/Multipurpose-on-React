2e2c30eb0ca0908db9fbb0f4fd8f274b38aa599c
Сергей Щуклин <wtorkanorka.shuclin@yandex.ru>
2022-10-30 06:54:48 +0500

    починил postcss но слетел в файлах со стилями prettier

--- postcss.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  plugins: [require('postcss-nested'), require('autoprefixer')],
-  plugins: postcss([require('postcss-nested')({ unwrap: ['phone'] })]),
-}
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,6 +1,41 @@
 import React from 'react'
-import SmartImage from '../../components/SmartImage/SmartImage'
-
+import styles from './Main.module.css'
 export default function HomePage() {
-   return <div>Home page</div>
+  return (
+    <div>
+      Home page
+      <ul className={styles['ul-style']}>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+      <ol>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ol>
+      <div className={styles['test']}>
+        <ul>
+          <li>
+            <nav>
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+              </ul>
+            </nav>
+          </li>
+          <li>
+            <nav>
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+              </ul>
+            </nav>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
