39db366525043c72314c93350263b6f88cfeb633
Сергей Щуклин <wtorkanorka.shuclin@yandex.ru>
2023-03-07 23:26:46 +0500

    build

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -9,14 +9,10 @@ type Props = Types & PropsWithChildren<HTMLAttributes<HTMLPictureElement>>
 export const Image = memo(({ styleParams, path, children, ...rest }: Props) => {
   return (
     <picture {...rest}>
-      <source srcSet={`./src/assets/${path}.avif`} type="image/avif" />
-      <source srcSet={`./src/assets/${path}.jpg`} type="image/jpg" />
-      <source srcSet={`./src/assets/${path}.webp`} type="image/webp" />
-      <img
-        src={`./src/assets/${path}.jpg`}
-        alt={`${path}`}
-        style={styleParams}
-      />
+      <source srcSet={`/assets/${path}.avif`} type="image/avif" />
+      <source srcSet={`/assets/${path}.jpg`} type="image/jpg" />
+      <source srcSet={`/assets/${path}.webp`} type="image/webp" />
+      <img src={`/assets/${path}.jpg`} alt={`${path}`} style={styleParams} />
     </picture>
   )
 })
