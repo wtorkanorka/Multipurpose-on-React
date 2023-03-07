5b86fe9707e3fd531548e75e94005bd374384614
Сергей Щуклин <wtorkanorka.shuclin@yandex.ru>
2023-03-07 18:57:37 +0500

    исправляю пути для картинок

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -9,23 +9,10 @@ type Props = Types & PropsWithChildren<HTMLAttributes<HTMLPictureElement>>
 export const Image = memo(({ styleParams, path, children, ...rest }: Props) => {
   return (
     <picture {...rest}>
-      <source
-        srcSet={`/Multipurpose-on-React/src/assets/${path}.avif`}
-        type="image/avif"
-      />
-      <source
-        srcSet={`/Multipurpose-on-React/src/assets/${path}.jpg`}
-        type="image/jpg"
-      />
-      <source
-        srcSet={`/Multipurpose-on-React/src/assets/${path}.webp`}
-        type="image/webp"
-      />
-      <img
-        src={`/Multipurpose-on-React/src/assets/${path}.jpg`}
-        alt={`${path}`}
-        style={styleParams}
-      />
+      <source srcSet={`src/assets/${path}.avif`} type="image/avif" />
+      <source srcSet={`src/assets/${path}.jpg`} type="image/jpg" />
+      <source srcSet={`src/assets/${path}.webp`} type="image/webp" />
+      <img src={`src/assets/${path}.jpg`} alt={`${path}`} style={styleParams} />
     </picture>
   )
 })
